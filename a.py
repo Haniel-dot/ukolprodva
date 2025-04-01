0f2f6d3a1d800df589ada84287860b5210030591
unknown <huyen.le@infis.cz>
2025-04-01 08:37:22 +0200

    prvni

---
+++ a.py
@@ -0,0 +1,10 @@
+#hanca_ukola
+j1=input("zadejte prvni jidlo co jste jedl")
+j2=input("zadejte druhe")
+j3=input("zadejte treti")
+j1k=int(input("a kolik ma kalorii"))
+j2k=int(input("kalorie druheho"))
+j3k=int(input("kalorie tretiho"))
+
+celkova_kalorie=j1k+j2k+j3k
+print(celkova_kalorie)
